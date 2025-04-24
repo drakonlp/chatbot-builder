f9d7ab4a33293465c66690064cc3dfae9970bfde
Seu Nome <seu@email.com>
2025-04-24 01:29:12 -0300

    Projeto inicial chatbot fullstack

--- frontend/src/components/ChatbotBuilder.jsx
+++ frontend/src/components/ChatbotBuilder.jsx
@@ -1,9 +1,1 @@
-import React from 'react';
-
-export default function ChatbotBuilder() {
-  return (
-    <div className="p-4">
-      <h1>Construtor de Chatbot</h1>
-    </div>
-  );
-}
+// React component ChatbotBuilder
